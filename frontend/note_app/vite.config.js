8f2fc0668e29f7d8a67cd25a51e243c055e612f0
Muhamed-Ahmed0 <138612671+Muhamed-Ahmed0@users.noreply.github.com>
2024-11-24 13:51:31 +0200

    Update vite.config.js

--- frontend/note_app/vite.config.js
+++ frontend/note_app/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  root: 'frontend/note_app', // Specify the correct root directory
   plugins: [react()],
 })
