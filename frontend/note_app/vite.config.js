78c214e955ddcbaac3a2166fe87bd9b2dcae0ea8
Muhamed-Ahmed0 <138612671+Muhamed-Ahmed0@users.noreply.github.com>
2024-11-24 14:12:06 +0200

    Update vite.config.js

--- frontend/note_app/vite.config.js
+++ frontend/note_app/vite.config.js
@@ -1,7 +1,7 @@
+// Just use the default root (no need to specify it)
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: "../../frontend/note_app", // Ensure the root points to the correct folder
 });
